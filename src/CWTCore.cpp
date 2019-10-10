948df5dd78b8a954765f3e21eac57cea5f4d830b
Andrew Gutekanst <andrew.gutekanst@gmail.com>
2019-10-09 23:26:15 -0700

    Add ASLR support

--- src/CWTCore.cpp
+++ src/CWTCore.cpp
@@ -2,7 +2,7 @@
 
 namespace CWT {
 	cube::Game* GetGamePtr() {
-		auto global = reinterpret_cast<cube::Game**>(0x140551A80);
+		auto global = reinterpret_cast<cube::Game**>(((uint64_t)GetModuleHandle(NULL))+0x551A80);
 		if (*global != nullptr) {
 			return *global;
 		}
