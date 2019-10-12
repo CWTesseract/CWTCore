fd21fd64501f4f4a8b2c4803277b468df699a9d8
Andrew Gutekanst <andrew.gutekanst@gmail.com>
2019-10-11 17:46:42 -0700

    Update cube::Host

--- include/CWTCore/cube/Host.h
+++ include/CWTCore/cube/Host.h
@@ -6,6 +6,7 @@
 #include <list>
 
 #include "CWTCore/cube/World.h"
+#include "CWTCore/common/msvc.h"
 
 namespace CWT {
 	namespace cube {
@@ -14,9 +15,8 @@ namespace CWT {
 			virtual ~Host() {};
 
 			World host_world;
-			int64_t field_550;
-			int64_t some_generation_thread_obj;
-			int64_t field_560;
+			bool some_bool;
+			msvc_Thrd_t some_generation_thread_obj;
 			std::map<void*, void*> connected_clients_maybe; // Node size 0x30
 			std::map<void*, void*>  invited_clients_list_or_map; // Node size 0x28
 			std::map<void*, void*> unk_list_0x588; // Node size 0x58
