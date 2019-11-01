5825caff213602b82251c75e4e1af24bd2343dfc
Andrew Gutekanst <andrew.gutekanst@gmail.com>
2019-10-31 17:57:26 -0700

    Update cube::Item fields

--- include/CWTCore/cube/Item.h
+++ include/CWTCore/cube/Item.h
@@ -18,11 +18,11 @@ namespace CWT {
 			uint8_t category;
 			int32_t id;
 			int32_t model;
-			int32_t level_req_maybe;
-			int32_t field_10;
+			int32_t region_x;
+			int32_t region_y;
 			byte stars;
 			int32_t field_18;
-			uint8_t Material;
+			uint8_t material;
 			ItemCustomization customizations[32];
 			uint8_t customization_count;
 			uint8_t field_9E;
